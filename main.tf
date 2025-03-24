c6b37badee1b1ab81fdf9c3316f3929a5239672c
Dmytro Mazurev <126726495+HotoMazur@users.noreply.github.com>
2025-03-24 15:32:21 +0200

    Fix 4

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "alb" {
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
-  security_group = [module.blog_sg.security_group_id]
+  security_group = [module.blog_sg.security_groups]
 
   listeners = {
     blog-http = {
