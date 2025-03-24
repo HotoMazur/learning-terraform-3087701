64c3830a2478ee65007c04735619556664498527
Dmytro Mazurev <126726495+HotoMazur@users.noreply.github.com>
2025-03-24 16:24:57 +0200

    Refactor to var

--- variables.tf
+++ variables.tf
@@ -2,3 +2,37 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.micro"
 }
+
+
+variable "ami_filter" {
+	type = object({
+		name  = string
+		owner = string
+	})
+
+	default = {
+  name   = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+  owners = "979382823631" # Bitnami
+	}
+}
+
+variable "environment" {
+
+	type = object({
+		name           = string
+		network_prefix = string
+	})
+
+	default = {
+		name = "dev"
+		network_prefix = "10.0"
+	}
+}
+
+variable "asg_min_size" {
+  default = 1
+}
+
+variable "asg_max_size" {
+	default = 2
+}
