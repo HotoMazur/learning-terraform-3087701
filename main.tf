0ea2cb97cb858c72880acc46993accd3c52b819c
Dmytro Mazurev <126726495+HotoMazur@users.noreply.github.com>
2025-03-24 15:46:33 +0200

    Fix 9

--- main.tf
+++ main.tf
@@ -43,37 +43,38 @@ resource "aws_instance" "blog" {
 
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
-  version = "~> 9.0"
+  version = "~> 6.0"
 
   name    = "blog-alb"
+
+  load_balancer_type = "application"
+
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  listeners = {
-    blog-http = {
-      port     = 80
-      protocol = "HTTP"
-      forward = {
-        target_group_key = "blog-instance"
-      }
-    }
-  }
-
-  target_groups = {
-    blog-instance = {
+  target_groups = [
+    {
       name_prefix      = "blog-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
       targets = {
-        blog_instance = {
+        my_instance = {
           target_id = aws_instance.blog.id
           port      = 80
         }
       }
     }
-  }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port     = 80
+      protocol = "HTTP"
+      target_group_index = 0
+    }
+  ]
 
   tags = {
     Environment = "dev"
